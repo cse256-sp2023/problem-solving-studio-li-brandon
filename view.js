c8c0b58de911609303f1ab94910072591fa1af21
Brandon Li <li.brandon@outlook.com>
2023-03-21 20:43:55 -0500

    Update Studio

--- view.js
+++ view.js
@@ -1,84 +1,103 @@
 // ---- Define your dialogs  and panels here ----
-first_permissions_panel = define_new_effective_permissions(
-  "first_panel",
-  true,
-  null
-);
+new_perms_out = define_new_effective_permissions("new_perms", true, null);
 
-user_selector = define_new_user_select_field("first_panel", "Select User");
+$('#sidepanel').append(new_perms_out)
+$('#new_perms').attr('filepath', '/C')
+$('#new_perms').attr('username', 'administrator')
+// $('#new_perms').attr('username', 'selected_user')
 
+selected_user_out = define_new_user_select_field("new_user", "select user", function(selected_user){
+    $('#new_perms').attr('filepath', '/C/presentation_documents/important_file.txt')
+    $('#new_perms').attr('username', selected_user)
+})
+$('#sidepanel').append(selected_user_out)
+
+
+dialogue_out = define_new_dialog("new_dialogue", "My Dialogue")
+$('.perm_info').click(function(){
+
+    // stuff that should happen on click goes here
+
+    console.log($('#new_perms').attr('filepath'))
+    console.log($('#new_perms').attr('username'))
+    console.log($( this ).attr('permission_name'))
+    dialogue_out.dialog('open')
+    if( allow_user_action(path_to_file[$('#new_perms').attr('filepath')], 
+    all_users[$('#new_perms').attr('username')], 
+    p)) {
+        // This action is allowed. Find the checkbox cell and put a checkbox there.
+        $('#new_dialogue').empty();
+        $('#new_dialogue').append(get_explanation_text($( this ).attr('permission_name')));
+    }
+
+})
 // ---- Display file structure ----
 
 // (recursively) makes and returns an html element (wrapped in a jquery object) for a given file object
 function make_file_element(file_obj) {
-  let file_hash = get_full_path(file_obj);
+    let file_hash = get_full_path(file_obj)
 
-  if (file_obj.is_folder) {
-    let folder_elem = $(`<div class='folder' id="${file_hash}_div">
+    if(file_obj.is_folder) {
+        let folder_elem = $(`<div class='folder' id="${file_hash}_div">
             <h3 id="${file_hash}_header">
                 <span class="oi oi-folder" id="${file_hash}_icon"/> ${file_obj.filename} 
                 <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
                     <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> 
                 </button>
             </h3>
-        </div>`);
+        </div>`)
 
-    // append children, if any:
-    if (file_hash in parent_to_children) {
-      let container_elem = $("<div class='folder_contents'></div>");
-      folder_elem.append(container_elem);
-      for (child_file of parent_to_children[file_hash]) {
-        let child_elem = make_file_element(child_file);
-        container_elem.append(child_elem);
-      }
+        // append children, if any:
+        if( file_hash in parent_to_children) {
+            let container_elem = $("<div class='folder_contents'></div>")
+            folder_elem.append(container_elem)
+            for(child_file of parent_to_children[file_hash]) {
+                let child_elem = make_file_element(child_file)
+                container_elem.append(child_elem)
+            }
+        }
+        return folder_elem
     }
-    return folder_elem;
-  } else {
-    return $(`<div class='file'  id="${file_hash}_div">
+    else {
+        return $(`<div class='file'  id="${file_hash}_div">
             <span class="oi oi-file" id="${file_hash}_icon"/> ${file_obj.filename}
             <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
                 <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> 
             </button>
-        </div>`);
-  }
+        </div>`)
+    }
 }
 
-for (let root_file of root_files) {
-  let file_elem = make_file_element(root_file);
-  $("#filestructure").append(file_elem);
+for(let root_file of root_files) {
+    let file_elem = make_file_element(root_file)
+    $( "#filestructure" ).append( file_elem);    
 }
+
+
 
 // make folder hierarchy into an accordion structure
-$(".folder").accordion({
-  collapsible: true,
-  heightStyle: "content",
-}); // TODO: start collapsed and check whether read permission exists before expanding?
+$('.folder').accordion({
+    collapsible: true,
+    heightStyle: 'content'
+}) // TODO: start collapsed and check whether read permission exists before expanding?
+
 
 // -- Connect File Structure lock buttons to the permission dialog --
 
 // open permissions dialog when a permission button is clicked
-$(".permbutton").click(function (e) {
-  // Set the path and open dialog:
-  let path = e.currentTarget.getAttribute("path");
-  perm_dialog.attr("filepath", path);
-  perm_dialog.dialog("open");
-  //open_permissions_dialog(path)
+$('.permbutton').click( function( e ) {
+    // Set the path and open dialog:
+    let path = e.currentTarget.getAttribute('path');
+    perm_dialog.attr('filepath', path)
+    perm_dialog.dialog('open')
+    //open_permissions_dialog(path)
 
-  // Deal with the fact that folders try to collapse/expand when you click on their permissions button:
-  e.stopPropagation(); // don't propagate button click to element underneath it (e.g. folder accordion)
-  // Emit a click for logging purposes:
-  emitter.dispatchEvent(
-    new CustomEvent("userEvent", {
-      detail: new ClickEntry(
-        ActionEnum.CLICK,
-        e.clientX + window.pageXOffset,
-        e.clientY + window.pageYOffset,
-        e.target.id,
-        new Date().getTime()
-      ),
-    })
-  );
+    // Deal with the fact that folders try to collapse/expand when you click on their permissions button:
+    e.stopPropagation() // don't propagate button click to element underneath it (e.g. folder accordion)
+    // Emit a click for logging purposes:
+    emitter.dispatchEvent(new CustomEvent('userEvent', { detail: new ClickEntry(ActionEnum.CLICK, (e.clientX + window.pageXOffset), (e.clientY + window.pageYOffset), e.target.id,new Date().getTime()) }))
 });
 
+
 // ---- Assign unique ids to everything that doesn't have an ID ----
-$("#html-loc").find("*").uniqueId();
+$('#html-loc').find('*').uniqueId() 
